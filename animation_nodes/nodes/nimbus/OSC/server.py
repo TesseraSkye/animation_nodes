a45aa61d2f4d5633da18ec51c6fb560ac92d3887
TesseraSkye <tesseraskye@gmail.com>
2019-01-22 18:10:04 -0700

    Pushing recent changes..

--- animation_nodes/nodes/nimbus/OSC/server.py
+++ animation_nodes/nodes/nimbus/OSC/server.py
@@ -19,285 +19,298 @@ from ....events import propertyChanged
 from ....base_types import AnimationNode
 ####
 
-####DECLARATIONS####
-a_parser = argparse.ArgumentParser()
-disp = dispatcher.Dispatcher()
-####################
-
-########ARGS########
-a_parser.add_argument(
-    "--ip",
-    default="127.0.0.1",
-    help="The ip the server listens on.",
-)
-a_parser.add_argument(
-    "--port",
-    type=int,
-    default=5505,
-    help="The port the server listens on.",
-)
-args = a_parser.parse_args()
-####################
-
-########VARS########
-# The address that the server is currently running on. Should never be changed.
-up_address = [None, None]  # [next_address[0], next_address[1]]
-# The next address the server will use. This is what the user sees.
-next_address = [None, None]
-# A unique address created when the server is started, and destroyed when it is killed. Used for internal addressing.
-uid = None
-# The threading osc server's 'object address'.
-server = None
-# The thread's 'object address'. The server is always run in a seperate thread so it doesn't block.
-thread = None
-# The uclient's 'object address' used to send the kill signal to the osc server
-killer_client = None
-####################
-
-#####DISPATCHER#####
-# Sets up dispatcher, which is used to map commands.
-
-####
-s_codes = {  # Use this to set up mapings
-
-    "0": "Closed normally."
-
-}
-####
-
-
-def getStopCodeDef(stop_code):
-    stop_code = str(stop_code)
-    try:
-        return s_codes[stop_code]
-    except KeyError:
-        return "Stop code does not exist."
-
-
-# The shutdown routine used to stop the server. Has stopcodes, default safe is 0.
-
-
-def dispShutdown(unused_addr, code):
-    print(
-        """
-        Shutdown requested with stop code {0}.\n
-        Code {0}: {1}\n
-        Terminating...
-        """.format(code, getStopCodeDef(code))
-    )
-
-#########ADDRESSING#########
-
-
-def setAddress(ip="", port=""):
-    """
-    Sets defaults which are used the next time the server is started.\n
-    usage: set(ip (str), port (int))\n
-    if None is entered, it defaults to\n
-    an ip of '127.0.0.1' and a port of 5505.\n
-    if one or both of the arguments is left blank,\n
-    that parameter will remain unchanged.
-    """
-
-    global next_address
-
-    try:
-        port = int(port)
-    except ValueError:
-        pass
-
-    if ip == None:
-        next_address[0] = args.ip
-    if port == None:
-        next_address[1] = args.port
-    if ip == "":
-        pass
-    if port == "":
-        pass
-    if ip != None and ip != "":
-        next_address[0] = ip
-    if port != None and port != "":
-        next_address[1] = port
-
-
-def getNextAddress(call):
-    """
-    Gets the next address
-    that will be used to start the server.
-    Args can be 'ip' and / or 'port'.\n
-    Returns None if server is not online.
-    """
-    global next_address
-
-    call = str(call)
-    call = call.lower()
-    print(call)
-    output = []
-
-    if "ip" in call:
-        output.append(next_address[0])
-    if "port" in (call):
-        output.append(next_address[1])
-    if not "ip" or "port" in call:
-        print(
-            """
-            No valid address selector listed!\n
-            Args can be 'ip' and / or 'port'.
-            """,
-        )
-        return None
-    return output
-
-
-def getCurrentAddress(call):
-    """
-    Gets the address of the running server, if anything.
-    Args can be 'ip' and / or 'port'.\n
-    Returns None if server is not online.
-    """
-    global up_address
-
-    call = str(call)
-    call = call.lower()
-    print(call)
-    output = []
-
-    if isUp() == True:
-        if "ip" in call:
-            output.append(up_address[0])
-        if "port" in (call):
-            output.append(up_address[1])
-        if not "ip" or "port" in call:
-            print(
-                """
-                No valid address selector listed!\n
-                Args can be 'ip' and or 'port'.
-                """,
-            )
-        return output
-
-    if isUp() != True:
-        print("Server not running!")
-        return None
-
-
-def getAddress(text=""):
-    """
-    Shortener function for getCurrent()
-    """
-    return getCurrentAddress(text)
-
-############################
-
-
-#########FUNCTIONS##########
-
-def isUp():
-    global thread
-    try:
-        up_status = thread.is_alive()
-        return up_status
-    except AttributeError:
-        print("thread doesn't point to anything! Is the server initialised?")
-
-
-def start():
-    """
-    This starts the OSC Server. It has no usable derivatives. \n
-    Can not be run if server is already up, will return False.\n
-    Takes no arguments.\n
-    Returns, as a list:\n
-    ..The server's status as a boolean\n
-    ..The Unique Identifier as an int\n
-    ..The ip and port as a nested list
-    """
-    global uid
-    global disp
-    global server
-    global thread
-    global up_address
-    global next_address
-
-    temp_up_status = isUp()
-    if temp_up_status != True:
-        if next_address[0] == None:
-            # antiquated, but can protect in case of bad things
-            next_address[0] = args.ip
-        if next_address[1] == None:
-            # antiquated, but can protect in case of bad things
-            next_address[1] = args.port
-
-        server = None  # just in case...
-        thread = None  # ^ ditto
-
-        # this is done here so that the UID can be generated in time for the dispatcher to pick up on it.
-        print("Binding current ip and port to up_address[0, 1]..")
-        up_address[0] = next_address[0]
-        up_address[1] = next_address[1]
-
-        print("Adding address to up_list..")
-        uid = up_tools.pushAddress(up_address)
-
-        ########
-        print(
-            "seting up stop command as '/nimbus/server/{server_identifier}/stop'".format(uid))
-        # Redirect for server shutdown.
-        disp.map(
-            "/nimbus/server/{server_identifier}/stop".format(uid), dispShutdown)
-        ########
-
-        server = osc_server.ThreadingOSCUDPServer(
-            (next_address[0], next_address[1]),
-            disp,
-        )
-
-        thread = Thread(
-            # Calling it this way forces it to either work, or throw an exception.
-            target=server.serve_forever,  # No maybe!
-            name="nimbus_osc_server",
-            daemon=True,
-        )
-        print("Starting Server!")
-        thread.start()
-
-        return [isUp(), uid, up_address]
-
-    elif isUp() == True:
-        print("Server already running!")
-
-
-def stop(stop_code=0):
-    """
-    Used to kill the running server.\n
-    Usage: stop(stop_code = 0)
-    """
-    global up_address
-    global uid
-
-    if server != None:
-        if isUp() == True:
-            killer_client = udp_client.SimpleUDPClient(
-                up_address[0], up_address[1])
-            print("Sending Kill Signal to server on {0}...".format(
-                up_address[0] + up_address[1]))
-            killer_client.send_message(
-                "/nimbus/server/{server_identifier}/stop".format(uid), str(stop_code))
-            if isUp() == True:  # checks that the server actually stopped
-                time.sleep(1)
-                raise Exception("STOP FAILURE", "Server failed to stop!")
-        elif isUp() == False:
-            print("Server is already stopped.")
-        return isUp()
-    elif server == None:
-        print("Server has never been started!")
-
-
 ############NODE############
+
+
 class OSCServerNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_OSCServerNode"
     bl_label = "OSC Server"
 
-    onOffState = bpy.props.EnumProperty(  # on - off toggle
+    ####DECLARATIONS####
+    a_parser = argparse.ArgumentParser()
+    disp = dispatcher.Dispatcher()
+    ####################
+
+    ########ARGS########
+    a_parser.add_argument(
+        "--ip",
+        default="127.0.0.1",
+        help="The ip the server listens on.",
+    )
+    a_parser.add_argument(
+        "--port",
+        type=int,
+        default=5505,
+        help="The port the server listens on.",
+    )
+    args = a_parser.parse_args()
+    ####################
+
+    ######VARS######
+    # The address that the server is currently running on. Should never be changed.
+    up_address = [None, None]  # [next_address[0], next_address[1]]
+    # The next address the server will use. This is what the user sees.
+    next_address = [None, None]
+    # A unique address created when the server is started, and destroyed when it is killed. Used for internal addressing.
+    uid = None
+    # The threading osc server's 'object address'.
+    server = None
+    # The thread's 'object address'. The server is always run in a seperate thread so it doesn't block.
+    thread = None
+    # The uclient's 'object address' used to send the kill signal to the osc server
+    killer_client = None
+    ################
+
+    #####DISPATCHER#####
+    # Sets up dispatcher, which is used to map commands.
+
+    ####
+    s_codes = {  # Use this to set up mapings
+
+        "0": "Closed normally."
+
+    }
+    ####
+
+    def getStopCodeDef(self, stop_code):
+        stop_code = str(stop_code)
+        try:
+            return self.s_codes[stop_code]
+        except KeyError:
+            return "Stop code does not exist."
+
+    # The shutdown routine used to stop the server. Has stopcodes, default safe is 0.
+
+    def dispShutdown(self, unused_addr, code):
+        self.server.shutdown()
+        print([self.server, self.thread])
+        print(
+            """
+            Shutdown requested with stop code {0}.\n
+            Code {0}: {1}\n
+            Terminating...
+            """.format(code, self.getStopCodeDef(code))
+        )
+        while time.sleep(0.8) != None:
+            self.thread.join()
+        if self.thread.is_alive == True:  # checks that the server actually stopped
+            raise Exception("Looks like the server failed to stop!")
+
+    #########ADDRESSING#########
+
+    def setAddress(self, ip="", port=""):
+        """
+        Sets defaults which are used the next time the server is started.\n
+        usage: set(ip (str), port (int))\n
+        if None is entered, it defaults to\n
+        an ip of '127.0.0.1' and a port of 5505.\n
+        if one or both of the arguments is left blank,\n
+        that parameter will remain unchanged.
+        """
+
+        try:
+            port = int(port)
+        except ValueError:
+            pass
+
+        if ip == None:
+            self.next_address[0] = self.args.ip
+        if port == None:
+            self.next_address[1] = self.args.port
+        if ip == "":
+            pass
+        if port == "":
+            pass
+        if ip != None and ip != "":
+            self.next_address[0] = ip
+        if port != None and port != "":
+            self.next_address[1] = port
+
+    def getNextAddress(self, call):
+        """
+        Gets the next address
+        that will be used to start the server.
+        Args can be 'ip' and / or 'port'.\n
+        Returns None if server is not online.
+        """
+
+        call = str(call)
+        call = call.lower()
+        print(call)
+        output = []
+
+        if "ip" in call:
+            output.append(self.next_address[0])
+        if "port" in (call):
+            output.append(self.next_address[1])
+        if not "ip" or "port" in call:
+            print(
+                """
+                No valid address selector listed!\n
+                Args can be 'ip' and / or 'port'.
+                """,
+            )
+            return None
+        return output
+
+    def getCurrentAddress(self, call):
+        """
+        Gets the address of the running server, if anything.
+        Args can be 'ip' and / or 'port'.\n
+        Returns None if server is not online.
+        """
+
+        call = str(call)
+        call = call.lower()
+        print(call)
+        output = []
+
+        if self.isUp() == True:
+            if "ip" in call:
+                output.append(self.up_address[0])
+            if "port" in (call):
+                output.append(self.up_address[1])
+            if not "ip" or "port" in call:
+                print(
+                    """
+                    No valid address selector listed!\n
+                    Args can be 'ip' and or 'port'.
+                    """,
+                )
+            return output
+
+        if self.isUp() != True:
+            print("Server not running!")
+            return None
+
+    def getAddress(self, text=""):
+        """
+        Shortener function for getCurrentAddress()
+        """
+        return self.getCurrentAddress(text)
+
+    ################
+
+    #########FUNCTIONS##########
+
+    def isUp(self):
+        """
+        Checks if the server is up.\n
+        Usage: isUp(self)\n
+        Returns boolean.
+        """
+
+        try:
+            up_status = self.thread.is_alive()
+            return up_status
+        except AttributeError:
+            print("thread doesn't point to anything! Is the server initialised?")
+
+    def start(self):
+        """
+        This starts the OSC Server. It has no usable derivatives. \n
+        Can not be run if server is already up, will return False.\n
+        Usage: start(self)\n
+        Returns, as a list:\n
+        ..The server's status as a boolean\n
+        ..The Unique Identifier as an int\n
+        ..The ip and port as a nested list
+        """
+
+        temp_up_status = self.isUp()
+        if temp_up_status == False or temp_up_status == None:
+            if self.next_address[0] == None:
+                # antiquated, but can protect in case of bad things
+                self.next_address[0] = self.args.ip
+            if self.next_address[1] == None:
+                # antiquated, but can protect in case of bad things
+                self.next_address[1] = self.args.port
+
+            self.server = None  # just in case...
+            self.thread = None  # ^ ditto
+
+            # this is done here so that the UID can be generated in time for the dispatcher to pick up on it.
+            print("Binding current ip and port to up_address[0, 1]..")
+            self.up_address[0] = self.next_address[0]
+            self.up_address[1] = self.next_address[1]
+
+            print("Adding address to up_list..")
+            self.uid = up_tools.pushAddress(self.up_address)
+
+            ########
+            kill_code = "/blender/oscserver/{0}/stop".format(self.uid)
+            print(
+                "seting up stop command as {0}".format(kill_code))
+            # Redirect for server shutdown.
+            self.disp.map(kill_code, self.dispShutdown)
+            ########
+
+            self.server = osc_server.ThreadingOSCUDPServer(
+                (self.next_address[0], self.next_address[1]),
+                self.disp,
+            )
+
+            self.thread = Thread(
+                # Calling it this way forces it to either work, or throw an exception.
+                target=self.server.serve_forever,  # No maybe!
+                name="nimbus_osc_server",
+                daemon=True,
+            )
+
+            print(self.server)
+
+            print("Starting Server!")
+            self.thread.start()
+
+            print([self.isUp(), self.uid, self.up_address])
+
+            return [self.isUp(), self.uid, self.up_address]
+
+        elif temp_up_status == True:
+            print("Server already running!")
+
+    def stop(self, stop_code=0):
+        """
+        Used to kill the running server.\n
+        Usage: stop(stop_code = 0)\n
+        Returns self.isUp()
+        """
+        self.killer_client = udp_client.SimpleUDPClient(
+            self.up_address[0], self.up_address[1])
+        print(
+            "Sending Kill Signal to server on {0} with UID {1}...".format(
+                [self.up_address[0], self.up_address[1]],
+                up_tools.getUID(self.up_address),
+            ),
+        )
+        self.killer_client.send_message(
+            "/blender/oscserver/{0}/stop".format(up_tools.getUID(self.up_address)), str(stop_code))
+        time.sleep(1)
+        # checks that the server actually stopped
+        if up_tools.getUID(self.up_address) != None:
+            raise Exception("STOP FAILURE", "Server failed to stop!")
+        ############################################
+
+    #############################
+
+    #####SPEC######
+    def setState(self, context):
+        if self.state == "on":
+            self.start()
+
+        elif self.state == "off":
+            self.stop()
+
+    def setIp(self, context):
+        self.setAddress(self.propIp)
+        self.stop()
+
+    def setPort(self, context):
+        self.setAddress("", self.propPort)
+        self.stop()
+    ###############
+
+    state = bpy.props.EnumProperty(  # on - off toggle
         items=[
             ("off", "Off", "Server State: Off"),
             ("on", "On", "Server State: On"),
@@ -306,11 +319,11 @@ class OSCServerNode(bpy.types.Node, AnimationNode):
         description="Toggle the server's state",
         default="off",
         options={"SKIP_SAVE"},
-        update=propertyChanged,
+        update=setState,
     )
 
-    setIp = bpy.props.StringProperty(  # ip
-        name="Serving Ip",
+    propIp = bpy.props.StringProperty(  # ip
+        name="IP",
         description="""
                The ip address used when starting the server.\n
                Don't use an IP over 126.255.255.255 except for lopback.\n
@@ -319,10 +332,10 @@ class OSCServerNode(bpy.types.Node, AnimationNode):
         default="127.0.0.1",
         maxlen=15,
         options={"SKIP_SAVE"},
-        update=propertyChanged,
+        update=setIp,
     )
-    setPort = bpy.props.IntProperty(  # port
-        name="Serving port",
+    propPort = bpy.props.IntProperty(  # port
+        name="PORT",
         description="""
                 The port used when starting the server.\n
                 Integer from 1 to 65535.\n
@@ -334,35 +347,31 @@ class OSCServerNode(bpy.types.Node, AnimationNode):
         soft_min=1,
         soft_max=65535,
         options={"SKIP_SAVE"},
-        update=propertyChanged,
+        update=setPort,
     )
 
     def create(self):
         pass
 
     def draw(self, layout):
-        layout.prop(self, "onOffState")
-        layout.prop(self, "setIp")
-        layout.prop(self, "setPort")
+        layout.prop(self, "state")
+        layout.prop(self, "propIp")
+        layout.prop(self, "propPort")
 
     def execute(self):
 
-        if self.onOffState.off:
-            stop()
+        if self.state == "off":
+            self.stop()
+            pass
 
-        if self.onOffState.on:
-            start()
-
-        if self.setIp:
-            print("IP!")
-
-        if self.setPort:
-            print("PORT!!")
+        if self.state == "on":
+            self.start()
+            pass
 
 
-############################
-############################
-# General registration for most modules
+            ############################
+            ############################
+            # General registration for most modules
 if __name__ == "__main__":
     import os
     import sys
